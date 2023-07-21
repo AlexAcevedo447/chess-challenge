c47eeb530bfc700093511d1e6e9255402026a5f6
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2023-07-21 16:19:50 -0500

    These files are modified in order to style some components and render chessBoard

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ChessBoard from './components/ChessBoard';
+
+function App() {
+  return (
+    <div className="App">
+
+      <ChessBoard rows={8} columns={8} />
+
+    </div>
+  );
+}
+
+export default App;
